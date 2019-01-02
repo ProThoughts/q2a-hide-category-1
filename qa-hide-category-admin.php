ee8ac60b32f5c36bf5fd5b15a4ec53a8f49bcc02
pupi1985 <pupi1985@bitbucket.org>
2019-01-02 18:51:43 -0300

    Reformat code

--- qa-hide-category-admin.php
+++ qa-hide-category-admin.php
@@ -1,14 +1,15 @@
 <?php
-class qa_hide_category_admin {
 
+class qa_hide_category_admin
+{
 	function allow_template($template)
 	{
-		return ($template!='admin');
+		return ($template != 'admin');
 	}
 
-	function option_default($option) {
-
-		switch($option) {
+	function option_default($option)
+	{
+		switch ($option) {
 			case 'hide-plugin-text':
 				return 'Hide category by Default';
 			case 'hide-plugin-title':
@@ -17,74 +18,71 @@ class qa_hide_category_admin {
 				return '.hide { text-align:center;}';
 			default:
 				return null;
-
 		}
 	}
+
 	function admin_form(&$qa_content)
 	{
-
 		//	Process form input
 
 		$ok = null;
 		if (qa_clicked('hide_save_button')) {
-			foreach($_POST as $i => $v) {
+			foreach ($_POST as $i => $v) {
 
-				qa_opt($i,$v);
+				qa_opt($i, $v);
 			}
 
 			$ok = qa_lang('admin/options_saved');
-		}
-		else if (qa_clicked('hide_reset_button')) {
-			foreach($_POST as $i => $v) {
+		} else if (qa_clicked('hide_reset_button')) {
+			foreach ($_POST as $i => $v) {
 				$def = $this->option_default($i);
-				if($def !== null) qa_opt($i,$def);
+				if ($def !== null)
+					qa_opt($i, $def);
 			}
 			$ok = qa_lang('admin/options_reset');
-		}			
+		}
 		//	Create the form for display
 
 
 		$fields = array();
 		$fields[] = array(
-				'label' => 'Hide Title',
-				'tags' => 'NAME="hide-plugin-title"',
-				'value' => qa_opt('hide-plugin-title'),
-				'type' => 'text',
-				);
+			'label' => 'Hide Title',
+			'tags' => 'NAME="hide-plugin-title"',
+			'value' => qa_opt('hide-plugin-title'),
+			'type' => 'text',
+		);
 		$fields[] = array(
-				'label' => 'Hide Text',
-				'tags' => 'NAME="hide-plugin-text"',
-				'value' => qa_opt('hide-plugin-text'),
-				'type' => 'text',
-				);
+			'label' => 'Hide Text',
+			'tags' => 'NAME="hide-plugin-text"',
+			'value' => qa_opt('hide-plugin-text'),
+			'type' => 'text',
+		);
 
 
 		$fields[] = array(
-				'label' => 'Hide custom css',
-				'tags' => 'NAME="hide-plugin-css"',
-				'value' => qa_opt('hide-plugin-css'),
-				'type' => 'textarea',
-				'rows' => 20
-				);
+			'label' => 'Hide custom css',
+			'tags' => 'NAME="hide-plugin-css"',
+			'value' => qa_opt('hide-plugin-css'),
+			'type' => 'textarea',
+			'rows' => 20,
+		);
 
 
 		return array(
-				'ok' => ($ok && !isset($error)) ? $ok : null,
+			'ok' => ($ok && !isset($error)) ? $ok : null,
 
-				'fields' => $fields,
+			'fields' => $fields,
 
-				'buttons' => array(
-					array(
-						'label' => qa_lang_html('main/save_button'),
-						'tags' => 'NAME="hide_save_button"',
-					     ),
-					array(
-						'label' => qa_lang_html('admin/reset_options_button'),
-						'tags' => 'NAME="hide_reset_button"',
-					     ),
-					),
-			    );
+			'buttons' => array(
+				array(
+					'label' => qa_lang_html('main/save_button'),
+					'tags' => 'NAME="hide_save_button"',
+				),
+				array(
+					'label' => qa_lang_html('admin/reset_options_button'),
+					'tags' => 'NAME="hide_reset_button"',
+				),
+			),
+		);
 	}
-
-
 }
--- qa-hide-category-layer.php
+++ qa-hide-category-layer.php
@@ -1,89 +1,91 @@
 <?php
 
-class qa_html_theme_layer extends qa_html_theme_base {
-
-	function doctype()
+class qa_html_theme_layer extends qa_html_theme_base
+{
+	function initialize()
 	{
-		if($this->template == 'account') {
+		if ($this->template == 'account') {
 			// add hide form
 			$hide_form = $this->hide_form();
-			if($hide_form) {
-
+			if (!empty($hide_form)) {
 				$this->content['form_3'] = $hide_form;
 			}
-
 		}
-		qa_html_theme_base::doctype();
+		parent::initialize();
 	}
 
 	//change category id, look up in admin category
 	function q_list_items($q_items)
 	{
-		$userid=qa_get_logged_in_userid();
-				require_once QA_INCLUDE_DIR . 'db/metas.php';
-				if(qa_db_usermeta_get($userid, 'hide') == "1") {
-		foreach ($q_items as $q_item){
-		if($q_item['raw']['categoryid'] != 28) {
-		$this->q_list_item($q_item);
-		}
-		}
-		}
-		else {
-		qa_html_theme_base::q_list_items($q_items);
+		$userid = qa_get_logged_in_userid();
+		require_once QA_INCLUDE_DIR . 'db/metas.php';
+		if (qa_db_usermeta_get($userid, 'hide') == "1") {
+			foreach ($q_items as $q_item) {
+				if ($q_item['raw']['categoryid'] != 28) {
+					$this->q_list_item($q_item);
+				}
+			}
+		} else {
+			parent::q_list_items($q_items);
 		}
 	}
-	
-	function hide_form() {
-		//un comment for permissions level
-		//if ( qa_get_logged_in_level() >= QA_USER_LEVEL_EXPERT ) {
-		if($handle = qa_get_logged_in_handle()) {
+
+	/**
+	 * @return array|string
+	 */
+	function hide_form()
+	{
+		//		uncomment for permissions level
+		//		if (qa_get_logged_in_level() < QA_USER_LEVEL_EXPERT) {
+		//			return array();
+		//		}
+		if ($handle = qa_get_logged_in_handle()) {
 			require_once QA_INCLUDE_DIR . 'db/metas.php';
 			$userid = qa_get_logged_in_userid();
 			if (qa_clicked('hide_save')) {
-				if(qa_post_text('hide')){
-					qa_db_usermeta_set($userid, 'hide', qa_post_text('hide')) ;
-				}
-				else {
-					qa_db_usermeta_set($userid, 'hide', "0") ;
+				if (qa_post_text('hide')) {
+					qa_db_usermeta_set($userid, 'hide', qa_post_text('hide'));
+				} else {
+					qa_db_usermeta_set($userid, 'hide', "0");
 				}
 				$ok = 'Category Options Saved.';
 				//qa_redirect($this->request,array('ok'=>qa_lang_html('admin/options_saved')));
+			} else {
+				$ok = null;
 			}
 
 			//$ok = qa_get('ok')?qa_get('ok'):null;
 
 			$fields = array();
 			$fields['hide'] = array(
-					'label' => qa_opt('hide-plugin-text'),
-					'tags' => 'NAME="hide"',
-					'type' => 'checkbox',
-					'value' =>  qa_db_usermeta_get($userid, 'hide'),
-					);
-			$form=array(
+				'label' => qa_opt('hide-plugin-text'),
+				'tags' => 'NAME="hide"',
+				'type' => 'checkbox',
+				'value' => qa_db_usermeta_get($userid, 'hide'),
+			);
+			$form = array(
 
-					'ok' => ($ok && !isset($error)) ? $ok : null,
+				'ok' => (isset($ok) && !isset($error)) ? $ok : null,
 
-					'style' => 'tall',
+				'style' => 'tall',
 
-					'title' => '<a name="hide_text"></a>'.qa_opt('hide-plugin-title'),
+				'title' => '<a name="hide_text"></a>' . qa_opt('hide-plugin-title'),
 
-					'tags' =>  'action="'.qa_self_html().'#hide_text" method="POST"',
+				'tags' => 'action="' . qa_self_html() . '#hide_text" method="POST"',
 
-					'fields' => $fields,
+				'fields' => $fields,
 
-					'buttons' => array(
-						array(
-							'label' => qa_lang_html('main/save_button'),
-							'tags' => 'NAME="hide_save"',
-						     ),
-						),
-				   );
+				'buttons' => array(
+					array(
+						'label' => qa_lang_html('main/save_button'),
+						'tags' => 'NAME="hide_save"',
+					),
+				),
+			);
 			return $form;
-
 		}
-		//uncommen t for permissions level
-		//}
 	}
 
 }
+
 ?>
